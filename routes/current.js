660e4548a45203f39d3403cdb4aba114fb95fbea
jakkiraj <jakkiraj@brocade.com>
2016-04-23 22:57:22 -0700

    changed current

--- routes/current.js
+++ routes/current.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send("ok it works");
-  res.render('index', { title: 'currentFile' });
 });
 
 module.exports = router;
