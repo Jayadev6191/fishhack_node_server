b7cf9f10ccad38924eeaebef2cc3a8c4e3c50a82
jakkiraj <jakkiraj@brocade.com>
2016-04-24 04:05:58 -0700

    ttt

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,8 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-/* GET users listing. */
+/* POST users listing. */
 router.post('/', function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  // request.get('',function(error, response, body){
-
-  // });
 
   res.send('respond with a resource');
 });
